3ef5589ea28cec93f64c1ecb49b7badac4dd5c9d
Karthikeyan-n47 <karthikeyan.n47@gmail.com>
2024-06-20 21:13:24 +0530

    added categories in home page

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -7,11 +7,19 @@ export default function Header() {
         <span className="headerTitleSm">React & Node</span>
         <span className="headerTitleLg">Blog</span>
       </div> */}
-      <img
-        src="https://images.unsplash.com/photo-1445445202798-d2d86bd125df?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt=""
-        className="headerImg"
-      />
+      <div className="headerImgContainer">
+        <img
+          src="https://images.unsplash.com/photo-1445445202798-d2d86bd125df?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+          className="headerImg"
+        />
+      </div>
+      <div className="headerText">
+        SocialBlog is a generic blog that covers a wide range of topics,
+        including but not limited to lifestyle, technology, health, and
+        entertainment. Our goal is to provide informative and engaging content
+        to our readers, catering to a diverse audience with varying interests.
+      </div>
     </div>
   );
 }
--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from "react";
 import Post from "../post/Post";
 import "./posts.css";
+import axios from "../../axios";
+import { Link } from "react-router-dom";
 
 export default function Posts({ posts }) {
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    const getCategory = async () => {
+      try {
+        const res = await axios.get("/category");
+        setCategories(res?.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getCategory();
+  }, []);
   return (
-    <div className="posts">
-      {posts.map((p, i) => {
-        return <Post post={p} key={i} />;
-      })}
+    <div className="postsWrapper">
+      <div className="categoriesTitle">Categories:</div>
+      <div className="categoriesContainer">
+        {categories?.map((category, i) => {
+          return (
+            <Link to={`/?cat=${category?.name}`} className="link" key={i}>
+              <div className="categoriesWrapper">{category?.name}</div>
+            </Link>
+          );
+        })}
+      </div>
+      <div className="posts">
+        {posts.map((p, i) => {
+          return <Post post={p} key={i} />;
+        })}
+      </div>
     </div>
   );
 }
